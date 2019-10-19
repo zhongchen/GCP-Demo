ffce9b4b7b0995c5e4263d3ea1f2a7e90aaca192
Zhong Chen <zchen87@gmail.com>
2019-10-19 08:48:26 -0700

    Zhong file monitoring fix (#5)
    * fix the filtering

--- terraform/cloudbuild.tf
+++ terraform/cloudbuild.tf
@@ -2,10 +2,10 @@ resource "google_cloudbuild_trigger" "file_update" {
   description = "trigger for file update"
 
   trigger_template{
-    branch_name = "^master*"
+    branch_name = ".*"
     repo_name = "github_zhongchen_gcp-demo"
   }
 
-  included_files = ["*input*"]
+  included_files = ["data/input*"]
   filename = "data/cloudbuild.yaml"
 }
