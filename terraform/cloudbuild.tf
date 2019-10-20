d94ac0849861c322d69bcd9da36b468c42d6793a
zhong <zhongchen@google.com>
2019-10-20 09:48:24 -0700

    change the trigger

--- terraform/cloudbuild.tf
+++ terraform/cloudbuild.tf
@@ -5,7 +5,7 @@ resource "google_cloudbuild_trigger" "file_update" {
   description = "trigger for file update"
 
   trigger_template{
-    branch_name = ".*"
+    branch_name = "^master$"
     repo_name = local.repo
   }
 
