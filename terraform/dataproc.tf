e1496b60491f037bb04be6a1cfaaafd6ad73b147
zhong <zhongchen@google.com>
2019-12-02 10:41:18 -0800

    update dataproc tf

--- terraform/dataproc.tf
+++ terraform/dataproc.tf
@@ -1,28 +1,50 @@
+locals {
+  enable_dataproc = 1
+}
+
 resource "google_dataproc_cluster" "spark" {
-    name = "sparkcluster"
-    region = "us-central1"
+  name = "sparkcluster"
+  region = "us-central1"
 
-    labels = {
-        env = "dev"
+  labels = {
+    env = "dev"
+  }
+
+  cluster_config {
+    staging_bucket = "zhong-gcp"
+
+    master_config {
+      num_instances = 1
+      machine_type = "n1-standard-1"
+      disk_config {
+        boot_disk_size_gb = 20
+        boot_disk_type = ""
+      }
     }
 
-    cluster_config {
-        staging_bucket = "zhong-gcp"
-
-        master_config {
-            num_instances = 1
-            machine_type = "n1-standard-1"
-            disk_config {
-                boot_disk_size_gb = 20
-                boot_disk_type = ""
-            }
-
-        }
+    worker_config {
+      num_instances = 2
+      machine_type = "n1-standard-1"
+      disk_config {
+        boot_disk_size_gb = 20
+        boot_disk_type = ""
+      }
     }
 
+    preemptible_worker_config {
+      num_instances = 0
+    }
+
+    gce_cluster_config {
+//      network = ""
+//      subnetwork = ""
+//      service_account = ""
+      tags = ["dev"]
+    }
 
     initialization_action {
-            script      = "gs://dataproc-initialization-actions/stackdriver/stackdriver.sh"
-            timeout_sec = 500
-        }
+      script = "gs://dataproc-initialization-actions/stackdriver/stackdriver.sh"
+      timeout_sec = 500
+    }
+  }
 }
