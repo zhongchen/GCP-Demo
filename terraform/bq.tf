9e59961c595830d45a12546ed64826abb3e0caa9
zhong <zhongchen@google.com>
2020-02-19 19:31:46 -0800

    add clustered bq table

--- terraform/bq.tf
+++ terraform/bq.tf
@@ -16,3 +16,19 @@ resource "google_bigquery_table" "data" {
 
   schema = file("schema/sample.json")
 }
+
+resource "google_bigquery_table" "data_clustering" {
+  dataset_id = google_bigquery_dataset.samples.dataset_id
+  table_id = "data_clustering"
+
+  labels = {
+    env = "dev"
+  }
+
+  schema = file("schema/cluster.json")
+  time_partitioning {
+    type = "DAY"
+    field = "inserted_at"
+  }
+  clustering = ["source"]
+}
